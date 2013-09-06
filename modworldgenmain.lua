6055000cd2674a1a0d0eb1e98f81c19138823ea4
simplex <theemptystring@gmail.com>
2013-09-06 10:29:20 -0300

    Attempt to fix lifemare's worldgen crash

--- modworldgenmain.lua
+++ modworldgenmain.lua
@@ -1,7 +1,48 @@
-TheMod = GLOBAL.require("upandaway" .. '.wicker.init')(env)
+local rawget = GLOBAL.rawget
 
--- This enables us to access configurations through TUNING.UPANDAWAY (for backwards compatibility)
-TheMod:AddMasterConfigurationKey("UPANDAWAY")
-GLOBAL.assert( TUNING.UPANDAWAY )
 
-TheMod:Run("worldgen_main")
+local IS_WGEN = (rawget(GLOBAL, "SEED") ~= nil)
+
+
+local run_handler = (function()
+		if IS_WGEN then
+				return GLOBAL.xpcall
+		else
+				return function(f) return true, f() end
+		end
+end)()
+
+
+local status, err = run_handler(function()
+	TheMod = GLOBAL.require("upandaway" .. '.wicker.init')(env)
+
+	-- This enables us to access configurations through TUNING.UPANDAWAY (for backwards compatibility)
+	TheMod:AddMasterConfigurationKey("UPANDAWAY")
+	GLOBAL.assert( TUNING.UPANDAWAY )
+
+	TheMod:Run("worldgen_main")
+end, GLOBAL.require("debug").traceback)
+
+
+if IS_WGEN and not status then pcall(function()
+	local io = GLOBAL.require("io")
+	local os = GLOBAL.require("os")
+	
+	local now = os.date("%x %X")
+	
+	local f = io.open(MODROOT .. "upandaway_log.txt", "w")
+
+	io.stderr:write(tostring(f))
+
+	if status then
+		f:write("[", now, "] The mod ran successfully.\n")
+	else
+		f:write("[", now, "] The mod failed to run. The error follows:\n")
+		f:write(err, "\n")
+	end
+
+	f:close()
+end) end
+
+
+if not status then return GLOBAL.error(err, 0) end
