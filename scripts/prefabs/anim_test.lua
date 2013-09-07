ffff1f6e5f33edff5754bf7605fc47460cc9e5a2
debugman18 <thebandforums@yahoo.com>
2013-09-06 19:32:47 -0500

    Added test walk animation. It works!

--- scripts/prefabs/anim_test.lua
+++ scripts/prefabs/anim_test.lua
@@ -1,7 +1,7 @@
 local assets=
 {
     -- always have to declare what assets you’re loading and using
-    --Asset("ANIM", "anim/testcritter.zip"),  -- same name as the .scml
+    Asset("ANIM", "anim/testcritter.zip"),  -- same name as the .scml
 }
 
 local function fn(Sim)
@@ -9,9 +9,9 @@ local function fn(Sim)
     local trans = inst.entity:AddTransform()
     local anim = inst.entity:AddAnimState()
     
-    --anim:SetBank("testcritter") -- name of the animation root
-    --anim:SetBuild("testcritter")  -- name of the file
-    --anim:PlayAnimation("bounce", true) -- name of the animation
+    anim:SetBank("testcritter") -- name of the animation root
+    anim:SetBuild("testcritter")  -- name of the file
+    anim:PlayAnimation("bounce", true) -- name of the animation
     
     return inst
 end
