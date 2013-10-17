c5f517c10cd310d58c887d597b99d6ca79513b0a
simplex <theemptystring@gmail.com>
2013-10-17 18:41:44 +0100

    Added tracing capabilities to debugtools.lua

--- scripts/upandaway/debugtools.lua
+++ scripts/upandaway/debugtools.lua
@@ -27,3 +27,45 @@ _O.ClimbTo = (modrequire 'lib.climbing').ClimbTo
 _O.GetStaticGenerator = GetStaticGenerator
 
 _O.GetStaticGen = _O.GetStaticGenerator
+
+
+_O.trace_flow = (function()
+	local output_path = "trace.txt"
+
+	local f = nil
+
+	return function()
+		if f then return end
+
+		f = assert(io.open(output_path, "w"), "Can't open '" .. output_path .. "' for writing!")
+
+		f:write("Execution trace (", os.date("%F %X"), ")\r\n")
+
+		debug.sethook(
+			function(ev_type)
+				if ev_type == "line" then
+					local info = debug.getinfo(2, 'Sl')
+
+					f:write(
+						("Entered %s:%d\r\n"):format(
+							info.source or "?",
+							info.currentline or -1
+						)
+					)
+				elseif ev_type == "call" then
+					local info = debug.getinfo(2, 'Sn')
+
+					f:write(
+						("Calling %s %s function %s\r\n"):format(
+							info.namewhat or "",
+							info.what or "anonymous",
+							info.name or "?"
+						)
+					)
+				end
+
+				f:flush()
+			end,
+		'lc')
+	end
+end)()
