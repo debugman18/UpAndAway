35de36bbdc8206c3b599661134d869cc9b3a6f42
simplex <theemptystring@gmail.com>
2013-11-30 11:37:07 +0000

    Luggs lore.

--- code/resources/lore.lua
+++ code/resources/lore.lua
@@ -24,4 +24,13 @@ Mercurius's mind rebels against such stagnation.
 How these 'gods' can remain sane in such static is beyond understanding
 Static....Mercurius can feel a plan beginning to form.
 
+]=][=[
+
+Initial results: not satisfying
+Though the device was able to cause desired reaction in subject
+The excess energy short circuits the device after 1 minute 37 seconds.
+The turf beneath our feet has shown great absorbtion properties
+Gobbling up any nectar or ichor those slobs spill
+Perhaps if Mercurius grounds the device... 
+
 ]=]
