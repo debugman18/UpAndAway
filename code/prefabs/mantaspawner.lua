e854716e1532b5fd936aabc8e33f24562210b63a
debugman18 <thebandforums@yahoo.com>
2014-04-12 22:24:23 -0500

    Intrinsic randomness and improvements.

---
+++ code/prefabs/mantaspawner.lua
@@ -0,0 +1,34 @@
+BindGlobal()
+
+local assets =
+{
+    --Asset("ANIM", "anim/arrow_indicator.zip"),
+}
+
+local prefabs = 
+{
+    "manta",
+}
+
+local function CanSpawn(inst)
+    return true
+end
+
+local function fn(Sim)
+    local inst = CreateEntity()
+    local trans = inst.entity:AddTransform()
+    local anim = inst.entity:AddAnimState()
+
+    inst:AddComponent("periodicspawner")
+    inst.components.periodicspawner:SetRandomTimes(20, math.random(1,16))
+    inst.components.periodicspawner:SetPrefab("manta")
+    inst.components.periodicspawner:SetOnSpawnFn(OnSpawned)
+    inst.components.periodicspawner:SetSpawnTestFn(CanSpawn)
+    inst.components.periodicspawner:SetDensityInRange(20, 2)
+    inst.components.periodicspawner:Start()
+    inst.components.periodicspawner:SetOnlySpawnOffscreen(true)
+    
+    return inst
+end
+
+return Prefab( "cloudrealm/mantaspawner", fn, assets, prefabs) 
