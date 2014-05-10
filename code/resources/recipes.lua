23ffda00e11cdd2c592eff30be10b2681b520b52
simplex <theemptystring@gmail.com>
2014-05-10 23:22:26 +0100

    prototypable research_lectern fix

--- code/resources/recipes.lua
+++ code/resources/recipes.lua
@@ -48,7 +48,7 @@ local refiner = Recipe("refiner", { Ingredient("crystal_fragment_quartz", 6, "im
 refiner.atlas = "images/refiner.xml"
 refiner.sortkey = 1 --GLOBAL.Recipes.prefab.sortkey
 
-local research_lectern = Recipe("research_lectern", { Ingredient("goldnugget", 4), Ingredient("crystal_fragment_relic", 2, "images/inventoryimages/crystal_fragment_relic.xml"), Ingredient("thunder_log", 6, "images/inventoryimages/thunder_log.xml") }, RECIPETABS.SCIENCE, TECH.SCIENCE_TWO, "research_lectern_placer")
+local research_lectern = Recipe("research_lectern", { Ingredient("goldnugget", 4), Ingredient("crystal_fragment_relic", 2, "images/inventoryimages/crystal_fragment_relic.xml"), Ingredient("thunder_log", 6, "images/inventoryimages/thunder_log.xml") }, RECIPETABS.SCIENCE, TECH.FABLE_2, "research_lectern_placer")
 research_lectern.atlas = "images/inventoryimages/research_lectern.xml"
 research_lectern.sortkey = GLOBAL.Recipes.researchlab2.sortkey
 
