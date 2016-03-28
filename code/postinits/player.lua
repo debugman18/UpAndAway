81397e4eb430c9d882e4ab557328457ecf280cc8
simplex <theemptystring@gmail.com>
2016-03-28 18:06:13 -0300

    fixed climbingvoter_replica crash on load

--- code/postinits/player.lua
+++ code/postinits/player.lua
@@ -42,7 +42,7 @@ end
 
 TheMod:AddPlayerPostInit(function(player)
     if IsHost() then
-		player:AddComponent("climbingvoter")
+		-- player:AddComponent("climbingvoter")
         player:AddComponent("quester")
         --player:AddComponent("ua_camera")
         player:AddComponent("speechlistener")
