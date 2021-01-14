6badc91fa98f27d9d746d753a1ef702903c95a00
Debugman18 <thebandforums@yahoo.com>
2021-01-13 18:07:21 -0600

    Moved to gemcore.

--- modmain.lua
+++ modmain.lua
@@ -1,37 +1,7 @@
-local _G = GLOBAL
-local assert, error = _G.assert, _G.error
+mod_dependencies = {
+    {--GEMCORE
+        workshop = "workshop-1378549454",
+    },
+}
 
----
-
-modimport 'lib/use.lua'
-use 'lib.timing'
-
----
-
-local ElapsedTime = GetSingletonTimeMeasurer()
-
----
-
-TheMod = use 'start_wicker'
-
----
-
-modimport "assets.lua"
-modimport "prefabfiles.lua"
-
---RemapSoundEvent("dontstarve/music/music_FE", "upandaway/music/music_FE")
-
----
-
-TheMod:Say("Running main...")
-TheMod:Run("main")
-
----
-
---------------
-
-TheMod:Say( ("Finished loading in %.3f seconds."):format(ElapsedTime()) )
-
-AddPrefabPostInit("world_network", function(inst)
-	inst:AddComponent("climbingmanager")
-end)
+GLOBAL.SetupGemCoreEnv()
